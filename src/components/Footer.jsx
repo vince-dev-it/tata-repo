4277c277a555153d6186e7d4415186631fa0aa81
vince-dev023 <jan.vincentneal04@gmail.com>
2024-07-21 16:08:50 +0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import facebooks from "../assets/facebooks.png";
 import jobstreet from "../assets/Jobstreet.png";
 import linkedin from "../assets/icons8-linkedin-100.png";
+import beanslogo from "../assets/beanstp.png";
 
 function Footer() {
   return (
@@ -9,27 +10,20 @@ function Footer() {
       <div className="grid md:grid-cols-3 sm:grid-cols-1 justify-items-center py-10 bg-[#131313]">
         <div className="flex flex-row items-center gap-2">
           <a href="https://www.facebook.com/YourSensei04/">
-            <img src={facebooks} className="w-10" />
+            <img src={facebooks} className="w-8" />
           </a>
-          <a href="https://www.facebook.com/YourSensei04/">
-            <p className=" text-white pl-0 ">yoursensei04</p>
+          <a href="https://www.jobstreet.com.ph/profile/janvincentneal-toledo-WVT9qZwz59">
+            <img src={jobstreet} className="w-8 " />
+          </a>
+          <a href="https://www.linkedin.com/in/jan-vincent-neal-toledo">
+            <img src={linkedin} className="w-8" />
           </a>
         </div>
         <div className="flex flex-row items-center gap-2">
-          <a href="https://www.jobstreet.com.ph/profile/janvincentneal-toledo-WVT9qZwz59">
-            <img src={jobstreet} className="w-10 " />
-          </a>
-          <a href="https://www.jobstreet.com.ph/profile/janvincentneal-toledo-WVT9qZwz59">
-            <p className="text-white">Jan Vincent Neal Toledo</p>
-          </a>
+          <img src={beanslogo} className="w-24" />
         </div>
         <div className="flex flex-row items-center gap-2">
-          <a href="https://www.linkedin.com/in/jan-vincent-neal-toledo">
-            <img src={linkedin} className="w-10" />
-          </a>
-          <a href="https://www.linkedin.com/in/jan-vincent-neal-toledo">
-            <p className="text-white">Jan Vincent Neal Ramos Toledo</p>
-          </a>
+          <p className="font-poppins text-white">© Jan Vincent Neal Toledo</p>
         </div>
       </div>
     </>
