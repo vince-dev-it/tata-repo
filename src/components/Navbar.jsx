2cf3f1a8de9b5a3cf4a2f3ab183c387c3aa943b3
vince-dev023 <jan.vincentneal04@gmail.com>
2024-07-21 23:45:52 +0800

    another

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,10 @@ function Navbar() {
       <div className="flex flex-row justify-between px-8 pt-2 font-poppins align-middle pl-12 sticky top-0 bg-[#131313]">
         <div className="">
           <Link to={"/"}>
-            <img src={logobeans} className="md:w-32 sm:w-20 cursor-pointer" />
+            <img
+              src={logobeans}
+              className="md:w-32 md:flex hidden cursor-pointer"
+            />
           </Link>
         </div>
         <div className="flex flex-row py-6 pr-14 gap-7 font-semibold md:text-base lg:text-base text-xs cursor-pointer hover:*:text-green-400 transition-all">
